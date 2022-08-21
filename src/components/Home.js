fcce6d3c21ba0fa08253c9e65a9dbf4f208728c0
Gk1mtd <Gk1mtd@users.noreply.github.com>
2022-08-21 19:44:23 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,9 @@ function Home() {
       <h1>Clickbeat</h1>
     <h2>Hi, Metroknowme was in need of a faster server and a new name. You can just follow this Link down there</h2>
       <video muted={true} autoPlay={true} loop={true} src={logo} alt="logo2" />
-      <Link className="link-button" to="https://clickbeat.netlify.app">
+      <a href="https://clickbeat.netlify.app">
         go to Clickbeat.app here
-      </Link>
+      </a>
     </div>
   );
 }
